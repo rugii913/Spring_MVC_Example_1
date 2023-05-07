193173f8a4d317f373de0f800e9d813ffc5fc1bd
rugii913 <rugii913@gmail.com>
2023-05-07 12:07:27 +0900

    스프링 MVC 구조 이해 - 과거의 스프링 어탭터와 뷰 리졸버
    - Dispatcher Servlet 구조 살펴봄
     -> 스프링 부트는 DispatcherServlet을 자동으로 등록함(DispatcherServlet은 HttpServlet을 상속하는 클래스)
     - (HttpServlet의 자손이자, DispatcherServlet의 부모인 추상 클래스)FrameworkServlet.service()를 시작으로 여러 메서드가 호출되면서 DispatcherServlet.doDispatch()가 호출
     - DispatcherServlet의 핵심은 doDispatch() 메서드 ===>> 다시 한 번
       훑어볼 것
    - 지금은 거의 사용하지 않는 핸들러 매핑, 핸들러 어댑터, 뷰리졸버를
      살펴봄
     - 과거 버전 스프링 컨트롤러
       인터페이스(org.springframework.web.servlet.mvc.Controller) >
       @Controller 애노테이션과는 완전히 다른 것임을 인지하고 있어야함
     - HttpRequestHandler > 서블릿과 가장 유사한 형태의 핸들러
      -> 스프링MVC에서 사용하는 대표적인 핸들러 매핑 두 가지, 핸들러 어댑터
       세 가지 살펴봄
      -> 실제 스프링 MVC에서 어떻게 핸들러를 매핑하고 어떻게 어댑터를
     가져오는지 살펴본 것
     - 스프링 부트가 자동 등록하는 뷰 리졸버 두 가지 살펴봄
      - 스프링 부트는 InternalResourceViewResolver라는 뷰 리졸버를 자동으로
       등록함(JSP로 렌더 넘겨주는 뷰 리졸버)
      -> 이 때 설정정보 application.properties에 작성한 prefix, suffix
      정보를 사용함

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -3,13 +3,20 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-@ServletComponentScan
+@ServletComponentScan //서블릿 자동 등록
 @SpringBootApplication
 public class ServletApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
 	}
-
+	
+	//p.180 뷰 리졸버 관련 설명 -> 스프링이 아래 코드에서 할 일을 대신 해서 빈으로 자동 등록한 것
+//	@Bean
+//	ViewResolver internalResourceViewResolver() {
+//		return new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
+//	}
 }
